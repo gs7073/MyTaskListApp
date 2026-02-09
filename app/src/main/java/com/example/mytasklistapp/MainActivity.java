afb904f114edd79c6584e635da241d04ead9b241
Cyber_Mamriot <gs7073@bs.amalnet.k12.il>
2026-02-09 10:39:04 +0200

    cross line

--- app/src/main/java/com/example/mytasklistapp/MainActivity.java
+++ app/src/main/java/com/example/mytasklistapp/MainActivity.java
@@ -1,19 +1,40 @@
 package com.example.mytasklistapp;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Button;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private List<Task> taskList;
+    private TaskAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        taskList = new ArrayList<>();
+        EditText etNewTask = findViewById(R.id.etNewTask);
+        Button btnAddTask = findViewById(R.id.btnAddTask);
+        RecyclerView rvTasks = findViewById(R.id.rvTasks);
+
+        adapter = new TaskAdapter(taskList);
+        rvTasks.setAdapter(adapter);
+        rvTasks.setLayoutManager(new LinearLayoutManager(this));
+
+        btnAddTask.setOnClickListener(v -> {
+            String title = etNewTask.getText().toString();
+            if (!title.isEmpty()) {
+                taskList.add(new Task(title));
+                adapter.notifyItemInserted(taskList.size() - 1);
+                etNewTask.setText("");
+            }
+        });
     }
 }
